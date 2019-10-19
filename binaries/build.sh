71afcd480525aca487d45d788f62b74bf65c7906
Alf-André Walla <fwsgonzo@hotmail.com>
2019-10-20 00:13:27 +0200

    Add RISC-V 32I hello world

---
+++ binaries/build.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+set -e
+
+PATH=$PATH:$HOME/opt/xPacks/@xpack-dev-tools/riscv-none-embed-gcc/8.2.1-3.1.1/.content/bin
+export CC=$HOME/opt/xPacks/@xpack-dev-tools/riscv-none-embed-gcc/8.2.1-3.1.1/.content/bin/riscv-none-embed-gcc
+export CXX=$HOME/opt/xPacks/@xpack-dev-tools/riscv-none-embed-gcc/8.2.1-3.1.1/.content/bin/riscv-none-embed-g++
+
+OPTS="-O0"
+WARNS="-Wall -Wextra"
+ABI="-march=rv32im -mabi=ilp32"
+# Goal: rv32ig --> rv32igc (rv32imafdc)
+
+for i in "$@"
+do
+case $i in
+    --build)
+    shift # past argument with no value
+	$CXX -std=c++17 -nostdlib $WARNS $OPTS $1 -o $1.elf
+	exit 0
+    ;;
+    --dump)
+	shift
+	riscv-none-embed-objdump -d $1.elf
+    exit 0 # done here
+    ;;
+    --sanitize)
+    OPTS="$OPTS -fsanitize=address"
+    shift # past argument with no value
+    ;;
+	--clean)
+	shift
+    rm -f $1.elf
+    exit 0
+    ;;
+    *)
+          # unknown option
+		  echo "--dump, --sanitize, --clean"
+    ;;
+esac
+done
