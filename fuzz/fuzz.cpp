04ce3f7d050664c8f8bca4d617d5a304f1d476a4
Alf-André Walla <fwsgonzo@hotmail.com>
2019-11-02 23:13:28 +0100

    Fuzz machine more efficiently

--- fuzz/fuzz.cpp
+++ fuzz/fuzz.cpp
@@ -5,12 +5,16 @@
 extern "C"
 void LLVMFuzzerTestOneInput(const uint8_t* data, size_t len)
 {
-	const auto binary = std::vector<uint8_t> (data, data + len);
+	std::vector<uint8_t> binary {};
+	riscv::Machine<riscv::RISCV32> machine { binary };
+	// copy fuzzer data to 0x1000 and skip the zero-page
+	machine.memory.copy_to_guest(0x1000, data, len);
+	machine.cpu.registers().pc = 0x1000;
 	try {
-		riscv::Machine<riscv::RISCV32> machine { binary };
-		printf(">>> Machine loaded\n");
 		while (!machine.stopped()) {
 			machine.simulate();
+			// let's avoid loops
+			if (machine.cpu.registers().counter > 1000) break;
 		}
 	} catch (std::exception& e) {
 		//printf(">>> Exception: %s\n", e.what());
