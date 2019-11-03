fd542672f5f6f20fe653e7584d0a273c685f7997
Alf-André Walla <fwsgonzo@hotmail.com>
2019-11-04 00:13:24 +0100

    Update fuzzer to latest, enable debugging

--- fuzz/fuzz.cpp
+++ fuzz/fuzz.cpp
@@ -8,7 +8,7 @@ void LLVMFuzzerTestOneInput(const uint8_t* data, size_t len)
 	std::vector<uint8_t> binary {};
 	riscv::Machine<riscv::RISCV32> machine { binary };
 	// copy fuzzer data to 0x1000 and skip the zero-page
-	machine.memory.copy_to_guest(0x1000, data, len);
+	machine.copy_to_guest(0x1000, data, len);
 	machine.cpu.registers().pc = 0x1000;
 	try {
 		while (!machine.stopped()) {
