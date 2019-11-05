e7fbf26fc8d8abde53c249368f1a67002d652066
Alf-André Walla <fwsgonzo@hotmail.com>
2019-11-05 22:26:11 +0100

    Add baked-in index to web API demo

--- webapi/server.cpp
+++ webapi/server.cpp
@@ -1,12 +1,13 @@
 #include <httplib.h>
 #include <cstdio>
-#include <iostream>
+#include <sys/time.h>
 #include <memory>
 #include <stdexcept>
 #include <string>
 #include <array>
 #include <libriscv/machine.hpp>
 #include "syscalls.cpp"
+#include "webpage.h"
 
 static const char* ADDRESS = "0.0.0.0";
 static const uint16_t PORT = 1234;
@@ -17,11 +18,24 @@ static int create_folder(const std::string& folder);
 static int python_sanitize_compile(const std::string& project, const std::string& infile, const std::string& outfile);
 static int write_file(const std::string& file, const std::string& text);
 static std::vector<uint8_t> load_file(const std::string& filename);
+static uint64_t micros_now();
+
+inline void common_response_fields(httplib::Response& res, int status)
+{
+	res.status = status;
+	res.set_header("Access-Control-Allow-Origin", "*");
+	res.set_header("Access-Control-Expose-Headers", "*");
+}
 
 int main(void)
 {
     using namespace httplib;
     Server svr;
+
+	svr.Get("/", [](const Request& req, Response& res) {
+		res.set_content((const char*) webpage_html, webpage_html_len, "text/html");
+		res.status = 200;
+	});
 
     svr.Post("/exec", [](const Request& req, Response& res) {
 		static int request_ID = 0;
@@ -32,59 +46,70 @@ int main(void)
 		// create project folder
 		if (create_folder(project_folder) < 0) {
 			if (errno != EEXIST) {
-				res.set_content("Failed to create project folder", "text/plain");
-				res.status = 500;
+				common_response_fields(res, 200);
+				res.set_header("X-Error", "Failed to create project folder");
 				return;
 			}
 		}
 
 		// write code into project folder
 		if (write_file(project_folder + "/" + codefile, req.body) != 0) {
-			res.set_content("Failed to write codefile", "text/plain");
-			res.status = 500;
+			common_response_fields(res, 200);
+			res.set_header("X-Error", "Failed to write codefile");
 			return;
 		}
 
 		// sanitize + compile code
+		const uint64_t c0 = micros_now();
 		const int cc = python_sanitize_compile(project_folder, codefile, binaryfile);
 		if (cc != 0) {
+			common_response_fields(res, 200);
 			auto vec = load_file(project_folder + "/status.txt");
+			res.set_header("X-Error", "Compilation failed");
 			res.set_content((const char*) vec.data(), vec.size(), "text/plain");
-			res.status = 500;
 			return;
 		}
+		const uint64_t c1 = micros_now();
 
 		// load binary and execute code
 		auto binary = load_file(project_folder + "/" + binaryfile);
 		if (binary.empty()) {
-			res.set_content("Failed to open binary", "text/plain");
-			res.status = 500;
+			common_response_fields(res, 200);
+			res.set_header("X-Error", "Failed to open binary");
 			return;
 		}
+
+		// start out with this:
+
+		// go-time: create machine, execute code
+		const uint64_t t0 = micros_now();
 		State<4> state;
 		riscv::Machine<4> machine { binary };
 		machine.install_syscall_handler(64, {&state, &State<4>::syscall_write});
 		machine.install_syscall_handler(80, {&state, &State<4>::syscall_dummy});
 		machine.install_syscall_handler(93, {&state, &State<4>::syscall_exit});
 		machine.install_syscall_handler(214, {&state, &State<4>::syscall_brk});
+
 		try {
 			while (!machine.stopped()) {
 				machine.simulate();
-				if (UNLIKELY(machine.cpu.registers().counter > MAX_INSTRUCTIONS)) {
-					res.set_content("Error: Maximum instructions reached", "text/plain");
-					res.status = 500;
-					return;
+				if (UNLIKELY(machine.cpu.registers().counter >= MAX_INSTRUCTIONS)) {
+					res.set_header("X-Exception", "Maximum instructions reached");
+					break;
 				}
 			}
 		} catch (std::exception& e) {
-			res.set_content("Exception: " + std::string(e.what()), "text/plain");
-			res.status = 500;
-			return;
+			res.set_header("X-Exception", e.what());
 		}
+		const uint64_t t1 = micros_now();
+		const auto instructions = std::to_string(machine.cpu.registers().counter);
 
-        res.set_content(state.output, "text/plain");
+		common_response_fields(res, 200);
 		res.set_header("X-Exit-Code", std::to_string(state.exit_code));
-		res.status = 200;
+		res.set_header("X-Compile-Time", std::to_string(c1 - c0) + " micros");
+		res.set_header("X-Execution-Time", std::to_string(t1 - t0) + " micros");
+		res.set_header("X-Instruction-Count", instructions);
+		res.set_content(state.output, "text/plain");
     });
 
 	printf("Listening on %s:%u\n", ADDRESS, PORT);
@@ -101,7 +126,6 @@ int python_sanitize_compile(const std::string& project, const std::string& infil
 	const std::string cmd = "/usr/bin/python3 ../sanitize.py " + project + " " + infile + " " + outfile;
 	return system(cmd.c_str());
 }
-
 
 int write_file(const std::string& file, const std::string& text)
 {
@@ -129,4 +153,11 @@ std::vector<uint8_t> load_file(const std::string& filename)
     }
     fclose(f);
     return result;
+}
+
+uint64_t micros_now()
+{
+	struct timeval tv;
+	gettimeofday(&tv, NULL);
+	return tv.tv_sec * 1000000ul + tv.tv_usec;
 }
