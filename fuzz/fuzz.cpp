f3f8c87db3e1adbd5639f0e0ad4d63a2ca115130
Alf-André Walla <fwsgonzo@hotmail.com>
2019-11-13 19:25:51 +0100

    Update fuzzer to latest

--- fuzz/fuzz.cpp
+++ fuzz/fuzz.cpp
@@ -2,20 +2,23 @@
 #include <cstdint>
 #include <libriscv/machine.hpp>
 
+static std::vector<uint8_t> binary {};
+// we don't want to see unhandled syscall messages
+namespace riscv {
+	bool verbose_machine = false;
+}
+
+
 extern "C"
 void LLVMFuzzerTestOneInput(const uint8_t* data, size_t len)
 {
-	std::vector<uint8_t> binary {};
 	riscv::Machine<riscv::RISCV32> machine { binary };
 	// copy fuzzer data to 0x1000 and skip the zero-page
 	machine.copy_to_guest(0x1000, data, len);
 	machine.cpu.registers().pc = 0x1000;
 	try {
-		while (!machine.stopped()) {
-			machine.simulate();
-			// let's avoid loops
-			if (machine.cpu.registers().counter > 1000) break;
-		}
+		// let's avoid loops
+		machine.simulate(5000);
 	} catch (std::exception& e) {
 		//printf(">>> Exception: %s\n", e.what());
 	}
