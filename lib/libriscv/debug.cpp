6b5ca51417f8e20d336b829cda613439a80b9b6b
Alf-André Walla <fwsgonzo@hotmail.com>
2020-02-21 17:36:26 +0100

    lib: Verbose FP-registers should honor the setting

--- lib/libriscv/debug.cpp
+++ lib/libriscv/debug.cpp
@@ -212,9 +212,9 @@ void Machine<W>::print_and_pause()
     // Memory subsystem
     printf("[MEM PAGES %8zu / %8zu]\n", memory.pages_active(), memory.pages_total());
 	// Floating-point registers
-	//if (this->verbose_fp_registers) {
+	if (this->verbose_fp_registers) {
 		printf("%s", cpu.registers().flp_to_string().c_str());
-	//}
+	}
 
     while (execute_commands(cpu))
         ;
