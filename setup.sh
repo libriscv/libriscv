8c72f2a68d29754a6406566c15e88a5331aa9439
Alf-André Walla <fwsgonzo@hotmail.com>
2020-03-13 18:49:28 +0100

    EASTL doesn't support recursive clone

--- setup.sh
+++ setup.sh
@@ -2,7 +2,11 @@
 # sudo apt install npm
 # sudo npm install --global xpm
 # xpm install --global @xpack-dev-tools/riscv-none-embed-gcc@latest
-git submodule update --init --recursive
+git submodule update --init
+pushd lib/EASTL
+git submodule update --init
+popd
+pushd emulator
 mkdir -p build
 pushd build
 cmake .. && make -j4
