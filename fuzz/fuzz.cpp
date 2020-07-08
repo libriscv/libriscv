2007b649b117e2bdec55aafdd772f8a22d2223bb
Alf-André Walla <fwsgonzo@hotmail.com>
2020-07-08 22:09:37 +0200

    Update, improve fuzzing

--- fuzz/fuzz.cpp
+++ fuzz/fuzz.cpp
@@ -7,14 +7,15 @@ void LLVMFuzzerTestOneInput(const uint8_t* data, size_t len)
 {
 	static std::vector<uint8_t> bin {};
 	static riscv::Machine<riscv::RISCV32> machine { bin };
-	// we don't want to see unhandled syscall messages
-	riscv::verbose_machine = false;
 
-	// copy fuzzer data to 0x1000 and skip the zero-page
+	if (UNLIKELY(len == 0)) return;
+
+	// Copy fuzzer data to 0x1000 and skip the zero-page.
+	// Non-zero length guarantees that the page will be created.
 	machine.copy_to_guest(0x1000, data, len);
-	machine.cpu.registers().pc = 0x1000;
+	machine.cpu.jump(0x1000);
 	try {
-		// let's avoid loops
+		// Let's avoid loops
 		machine.simulate(5000);
 	} catch (std::exception& e) {
 		//printf(">>> Exception: %s\n", e.what());
