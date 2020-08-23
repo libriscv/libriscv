9cbe5798bfec55070f82813789cc93cab67dd478
Alf-André Walla <fwsgonzo@hotmail.com>
2020-08-23 20:25:47 +0200

    Override newlibs internal heap functions

--- binaries/barebones/libc/heap.cpp
+++ binaries/barebones/libc/heap.cpp
@@ -22,3 +22,26 @@ void free(void* ptr)
 {
 	return sys_free(ptr);
 }
+
+/* Newlib internal reentrancy-safe heap functions.
+   Our system calls are safe because they are atomic. */
+extern "C"
+void* _malloc_r(struct _reent*, size_t size)
+{
+	return sys_malloc(size);
+}
+extern "C"
+void* _calloc_r(struct _reent*, size_t count, size_t size)
+{
+	return sys_calloc(count, size);
+}
+extern "C"
+void* _realloc_r(struct _reent*, void* ptr, size_t newsize)
+{
+	return sys_realloc(ptr, newsize);
+}
+extern "C"
+void _free_r(struct _reent*, void* ptr)
+{
+	return sys_free(ptr);
+}
