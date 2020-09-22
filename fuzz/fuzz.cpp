6be34048b386d40da928a826eb80819b79815014
Alf-André Walla <fwsgonzo@hotmail.com>
2020-09-22 11:12:27 +0200

    Enable binary loader fuzzing by default

--- fuzz/fuzz.cpp
+++ fuzz/fuzz.cpp
@@ -2,11 +2,12 @@
 #include <cstdint>
 #include <libriscv/machine.hpp>
 
+static const std::vector<uint8_t> empty {};
+
 static void fuzz_instruction_set(const uint8_t* data, size_t len)
 {
-	static std::vector<uint8_t> bin {};
-	static riscv::Machine<riscv::RISCV32> machine32 { bin };
-	static riscv::Machine<riscv::RISCV64> machine64 { bin };
+	static riscv::Machine<riscv::RISCV32> machine32 { empty };
+	static riscv::Machine<riscv::RISCV64> machine64 { empty };
 
 	if (UNLIKELY(len == 0)) return;
 
@@ -17,7 +18,7 @@ static void fuzz_instruction_set(const uint8_t* data, size_t len)
 	try {
 		// Let's avoid loops
 		machine32.simulate(5000);
-	} catch (std::exception& e) {
+	} catch (const std::exception& e) {
 		//printf(">>> Exception: %s\n", e.what());
 	}
 	// Again for 64-bit
@@ -25,18 +26,22 @@ static void fuzz_instruction_set(const uint8_t* data, size_t len)
 	machine64.cpu.jump(0x1000);
 	try {
 		machine64.simulate(5000);
-	} catch (std::exception& e) {
+	} catch (const std::exception& e) {
 		//printf(">>> Exception: %s\n", e.what());
 	}
 }
 
 static void fuzz_elf_loader(const uint8_t* data, size_t len)
 {
-	std::vector<uint8_t> bin {data, data + len};
+	const std::vector<uint8_t> bin {data, data + len};
 	try {
-		riscv::Machine<riscv::RISCV32> machine32 { bin };
-		riscv::Machine<riscv::RISCV64> machine64 { bin };
-	} catch (std::exception& e) {
+		riscv::Machine<riscv::RISCV32> m32 { bin };
+	} catch (const std::exception& e) {
+		//printf(">>> Exception: %s\n", e.what());
+	}
+	try {
+		riscv::Machine<riscv::RISCV64> m64 { bin };
+	} catch (const std::exception& e) {
 		//printf(">>> Exception: %s\n", e.what());
 	}
 }
@@ -44,6 +49,6 @@ static void fuzz_elf_loader(const uint8_t* data, size_t len)
 extern "C"
 void LLVMFuzzerTestOneInput(const uint8_t* data, size_t len)
 {
-	//fuzz_elf_loader(data, len);
+	fuzz_elf_loader(data, len);
 	fuzz_instruction_set(data, len);
 }
