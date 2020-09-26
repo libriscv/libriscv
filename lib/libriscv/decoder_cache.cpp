f78dc97bbbddae255144e11689f7bc1ab0118c13
Alf-André Walla <fwsgonzo@hotmail.com>
2020-09-26 21:12:02 +0200

    Simplify and don't store executable and rodata pages when not necessary

--- lib/libriscv/decoder_cache.cpp
+++ lib/libriscv/decoder_cache.cpp
@@ -24,57 +24,33 @@ namespace riscv
 		this->m_exec_decoder =
 			decoder_array[0].template get_base<W>() - pbase / DecoderCache<Page::SIZE>::DIVISOR;
 		this->m_decoder_cache = &decoder_array[0];
-		size_t dcindex = 0;
-		while (len > 0)
-		{
-			const size_t size = std::min(Page::size(), len);
-			const size_t pageno = page_number(addr);
-			// find page itself
-			auto it = m_pages.find(pageno);
-			if (it != m_pages.end()) {
-				auto& page = it->second;
-				if (page.attr.exec) {
-					// assign slice
-					auto* cache = &decoder_array[dcindex];
-					dcindex++;
+
 #ifdef RISCV_INSTR_CACHE_PREGEN
-					// fill start with illegal instructions
-					const address_t start_offset = addr & (Page::size()-1);
-					for (address_t off = 0; off < start_offset; off += 4)
-					{
-						cache->template get<W> (off / cache->DIVISOR) =
-							machine().cpu.decode(rv32i_instruction{0}).handler;
-					}
-					// generate instruction handler pointers for machine code
-					for (address_t dst = addr; dst < addr + size;)
-					{
-						const address_t offset = dst & (Page::size()-1);
-						rv32i_instruction instruction;
-						instruction.whole = *(uint32_t*) (page.data() + offset);
+		const auto* exec_offset = m_exec_pagedata.get() - pbase;
+		// generate instruction handler pointers for machine code
+		for (address_t dst = pbase; dst < pbase + plen;)
+		{
+			const size_t cacheno = page_number(dst - pbase);
+			const address_t offset = dst & (Page::size()-1);
+			auto& cache = decoder_array[cacheno];
+			auto& entry = cache.template get<W> (offset / cache.DIVISOR);
 
-						cache->template get<W> (offset / cache->DIVISOR) =
-							machine().cpu.decode(instruction).handler;
+			if (dst >= addr && dst < addr + len)
+			{
+				rv32i_instruction instruction;
+				instruction.whole = *(uint32_t*) &exec_offset[dst];
 
-						dst += instruction.length();
-					}
-					// fill end with illegal instructions
-					const address_t end_offset = (addr + size) & (Page::size()-1);
-					if (end_offset != 0)
-					for (address_t off = end_offset; off < Page::size(); off += 4)
-					{
-						cache->template get<W> (off / cache->DIVISOR) =
-							machine().cpu.decode(rv32i_instruction{0}).handler;
-					}
-#else
-					// the instructions will be decoded on-demand
-					*cache = {};
-#endif
-				}
+				entry = machine().cpu.decode(instruction).handler;
+				dst += instruction.length();
+			} else {
+				entry = machine().cpu.decode({0}).handler;
+				dst += 4;
 			}
-
-			addr += size;
-			len  -= size;
 		}
+#else
+		// zero the whole thing
+		std::memset(decoder_array, 0, n_pages * sizeof(decoder_array[0]));
+#endif
 	}
 #endif
 
