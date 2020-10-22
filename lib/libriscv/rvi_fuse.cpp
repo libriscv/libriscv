a154fa00a60c752532a9ac2c98248f93ea6d168b
Alf-André Walla <fwsgonzo@hotmail.com>
2020-10-23 01:17:24 +0200

    Make sure we test unsigned fused syscall numbers

--- lib/libriscv/rvi_fuse.cpp
+++ lib/libriscv/rvi_fuse.cpp
@@ -5,7 +5,8 @@ T& view_as(rv32i_instruction& i) {
 }
 
 template <int W>
-inline bool fused_li_ecall(typename CPU<W>::instr_pair& i1, typename CPU<W>::instr_pair& i2, int sysno)
+inline void fused_li_ecall(
+	typename CPU<W>::instr_pair& i1, typename CPU<W>::instr_pair& i2, int sysno)
 {
 	union FusedSyscall {
 		struct {
@@ -26,7 +27,6 @@ inline bool fused_li_ecall(typename CPU<W>::instr_pair& i1, typename CPU<W>::ins
 		cpu.reg(RISCV::REG_ECALL) = fop.sysno;
 		cpu.machine().unchecked_system_call(fop.sysno);
 	};
-	return true;
 }
 
 template <int W>
@@ -38,21 +38,24 @@ bool CPU<W>::try_fuse(instr_pair i1, instr_pair i2) const
 		i2.first == DECODED_INSTR(SYSCALL).handler)
 	{
 		// fastest possible system calls
-		const auto sysno = i1.second.Itype.signed_imm();
+		const uint16_t sysno = i1.second.Itype.signed_imm();
 		if (i1.second.Itype.rd == RISCV::REG_ECALL && sysno < RISCV_SYSCALLS_MAX)
 		{
-			return fused_li_ecall<W>(i1, i2, sysno);
+			fused_li_ecall<W>(i1, i2, sysno);
+			return true;
 		}
 	}
 # ifdef RISCV_EXT_COMPRESSED
+	// C.LI + ECALL fused
 	else if (i1.first == DECODED_INSTR(C1_LI).handler &&
 		i2.first == DECODED_INSTR(SYSCALL).handler)
 	{
 		const auto ci = i1.second.compressed();
-		const auto sysno = ci.CI.signed_imm();
+		const uint16_t sysno = ci.CI.signed_imm();
 		if (ci.CI.rd == RISCV::REG_ECALL && sysno < RISCV_SYSCALLS_MAX)
 		{
-			return fused_li_ecall<W>(i1, i2, sysno);
+			fused_li_ecall<W>(i1, i2, sysno);
+			return true;
 		}
 	}
 # endif
