ea023d41e65b8923bcf6af1ea651ac4be2d2abb8
Alf-André Walla <fwsgonzo@hotmail.com>
2020-10-29 00:42:55 +0100

    Create ADDI/ADDI fused op

--- lib/libriscv/rvi_fuse.cpp
+++ lib/libriscv/rvi_fuse.cpp
@@ -23,7 +23,10 @@ inline void fused_li_ecall(
 	}.whole;
 	i1.first = [] (auto& cpu, rv32i_instruction instr) {
 		auto& fop = view_as<FusedSyscall> (instr);
-		cpu.registers().pc += fop.ilen;
+		if constexpr (compressed_enabled)
+			cpu.registers().pc += fop.ilen;
+		else
+			cpu.registers().pc += 4;
 		cpu.reg(RISCV::REG_ECALL) = fop.sysno;
 		cpu.machine().unchecked_system_call(fop.sysno);
 	};
@@ -42,6 +45,45 @@ bool CPU<W>::try_fuse(instr_pair i1, instr_pair i2) const
 		if (i1.second.Itype.rd == RISCV::REG_ECALL && sysno < RISCV_SYSCALLS_MAX)
 		{
 			fused_li_ecall<W>(i1, i2, sysno);
+			return true;
+		}
+	}
+	// ADDI x, x + ADDI y, y fused
+	if (i1.first == DECODED_INSTR(OP_IMM_ADDI).handler &&
+		i2.first == DECODED_INSTR(OP_IMM_ADDI).handler)
+	{
+		if (i1.second.Itype.rd == i1.second.Itype.rs1 && i1.second.Itype.rd < 16)
+		if (i2.second.Itype.rd == i2.second.Itype.rs1 && i2.second.Itype.rd < 16)
+		if constexpr (!compressed_enabled)
+		{
+			union FusedAddi {
+				struct {
+					uint32_t addi1 : 12;
+					uint32_t reg1  : 4;
+					uint32_t addi2 : 12;
+					uint32_t reg2  : 4;
+				};
+				static bool sign(uint32_t imm) {
+					return imm & 0x800;
+				}
+				static int64_t signed_imm(uint32_t imm) {
+					const uint64_t ext = 0xFFFFFFFFFFFFF000;
+					return imm | (sign(imm) ? ext : 0);
+				}
+				uint32_t whole;
+			};
+			FusedAddi fop;
+			fop.addi1 = i1.second.Itype.imm;
+			fop.reg1  = i1.second.Itype.rd;
+			fop.addi2 = i2.second.Itype.imm;
+			fop.reg2  = i2.second.Itype.rd;
+			i1.second.whole = fop.whole;
+			i1.first = [] (auto& cpu, rv32i_instruction instr) {
+				auto& fop = view_as<FusedAddi> (instr);
+				cpu.reg(fop.reg1) += FusedAddi::signed_imm(fop.addi1);
+				cpu.reg(fop.reg2) += FusedAddi::signed_imm(fop.addi2);
+				cpu.registers().pc += 4;
+			};
 			return true;
 		}
 	}
