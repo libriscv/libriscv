abb7d20abc1a1c38f4b3bb526650cb1e3278eb0e
Alf-André Walla <fwsgonzo@hotmail.com>
2020-12-05 12:39:00 +0100

    Some cleanup in native heap inline assembly

--- binaries/barebones/libc/heap.cpp
+++ binaries/barebones/libc/heap.cpp
@@ -2,6 +2,8 @@
 #include <include/libc.hpp>
 #include <cstdlib>
 
+#if 1
+
 extern "C"
 void* malloc(size_t size)
 {
@@ -20,7 +22,7 @@ void* realloc(void* ptr, size_t newsize)
 extern "C"
 void free(void* ptr)
 {
-	return sys_free(ptr);
+	sys_free(ptr);
 }
 
 /* Newlib internal reentrancy-safe heap functions.
@@ -43,5 +45,23 @@ void* _realloc_r(struct _reent*, void* ptr, size_t newsize)
 extern "C"
 void _free_r(struct _reent*, void* ptr)
 {
-	return sys_free(ptr);
+	sys_free(ptr);
 }
+
+#else
+
+static const uintptr_t sbrk_start = 0x40000000;
+static const uintptr_t sbrk_max   = sbrk_start + 0x1000000;
+
+extern "C"
+long _sbrk(uintptr_t new_end)
+{
+	static uintptr_t sbrk_end = sbrk_start;
+    if (new_end == 0) return sbrk_end;
+    sbrk_end = new_end;
+    sbrk_end = std::max(sbrk_end, sbrk_start);
+    sbrk_end = std::min(sbrk_end, sbrk_max);
+	return sbrk_end;
+}
+
+#endif
