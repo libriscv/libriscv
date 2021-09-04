2faa4e2c66483bdc8a2e2b270377826b9d2e65d0
Alf-André Walla <fwsgonzo@hotmail.com>
2021-09-04 18:27:29 +0200

    Add symlinks to the emulator programs

--- emulator/build.sh
+++ emulator/build.sh
@@ -3,11 +3,10 @@ set -e
 
 mkdir -p build
 pushd build
-cmake .. -DRISCV_DEBUG=OFF -DRISCV_ICACHE=ON
+cmake .. -DCMAKE_BUILD_TYPE=Release -DRISCV_EXPERIMENTAL=ON
 make -j
 popd
-mkdir -p build_debug
-pushd build_debug
-cmake .. -DRISCV_DEBUG=ON
-make -j
-popd
+
+ln -fs build/rvmicro .
+ln -fs build/rvnewlib .
+ln -fs build/rvlinux .
