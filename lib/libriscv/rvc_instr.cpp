896dc81d7c33e0c561dd52ee63b938caf85352af
Alf-André Walla <fwsgonzo@hotmail.com>
2021-09-12 15:46:33 +0200

    C.LUI is sign-extended, but do the shift unsigned

--- lib/libriscv/rvc_instr.cpp
+++ lib/libriscv/rvc_instr.cpp
@@ -201,7 +201,7 @@ namespace riscv
 	[] (auto& cpu, rv32i_instruction instr) {
 		const rv32c_instruction ci { instr };
 		// LUI rd, imm[17:12]
-		cpu.reg(ci.CI.rd) = ci.CI.signed_imm() << 12;
+		cpu.reg(ci.CI.rd) = (RVREGTYPE(cpu)) ci.CI.signed_imm() << 12;
 	}, DECODED_COMPR(C1_ADDI16SP).printer);
 
 	COMPRESSED_INSTR(C1_ALU_OPS,
