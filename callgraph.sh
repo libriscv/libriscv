929e984dc370edc2a8814cf9480935b2b5233fd9
Alf-André Walla <fwsgonzo@hotmail.com>
2022-02-03 19:46:52 +0100

    Update the callgraph generator script

--- callgraph.sh
+++ callgraph.sh
@@ -1,12 +1,28 @@
 #! /bin/bash
 set -e
-BINARY=$1
+EMULATOR="./emulator/rvlinux"
+BINARY=${1:-binaries/linux64/build/hello_world}
 
 echo -e "\n>>> Installing dependencies"
 sudo pip3 install setuptools wheel
 pip3 -q install gprof2dot
 sudo apt install -y graphviz
 
+echo -e "\n>>> Building RISC-V Linux emulator..."
+
+pushd emulator
+mkdir -p build
+pushd build
+cmake .. -G Ninja -DGPROF=ON -DCMAKE_BUILD_TYPE=Debug -DRISCV_EXPERIMENTAL=ON
+ninja
+popd
+
+echo -e "\n>>> Done"
+
+popd
+
 echo -e "\n>>> Generating callgraph.svg from $BINARY"
-gprof $BINARY | gprof2dot | dot -Tsvg -o callgraph.svg
+
+$EMULATOR $BINARY
+gprof $EMULATOR | gprof2dot | dot -Tsvg -o callgraph.svg
 firefox callgraph.svg
