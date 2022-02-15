d6a182f7e80d9736904a6664bce9169b5439ff72
Alf-André Walla <fwsgonzo@hotmail.com>
2022-02-15 22:50:34 +0100

    Do benchmarking using a callback function in WebAPI

--- webapi/execute.cpp
+++ webapi/execute.cpp
@@ -7,8 +7,9 @@ using namespace httplib;
 
 // Avoid endless loops, code that takes too long and excessive memory usage
 static const uint64_t MAX_BINARY       = 32'000'000UL;
-static const uint64_t MAX_INSTRUCTIONS = 6'000'000UL;
+static const uint64_t MAX_INSTRUCTIONS = 36'000'000UL;
 static const uint64_t MAX_MEMORY       = 32UL * 1024 * 1024;
+static const size_t   NUM_SAMPLES      = 50;
 
 static const std::vector<std::string> env = {
 	"LC_CTYPE=C", "LC_ALL=C", "USER=groot"
@@ -32,9 +33,10 @@ protected_execute(const Request& req, Response& res, const ContentReader& creade
 	}
 
 	// go-time: create machine, execute code
-	riscv::Machine<riscv::RISCV64> machine { binary, {
+	const riscv::MachineOptions<riscv::RISCV64> options {
 		.memory_max = MAX_MEMORY
-	}};
+	};
+	riscv::Machine<riscv::RISCV64> machine { binary, options };
 
 	machine.setup_linux({"program"}, env);
 	setup_linux_syscalls(machine);
@@ -45,20 +47,49 @@ protected_execute(const Request& req, Response& res, const ContentReader& creade
 		output.append(text, len);
 	});
 
-	struct StartupState {
-		bool break_used = false;
-	} startup_state;
-	machine.set_userdata(&startup_state);
+	struct BenchmarkState {
+		bool benchmark = false;
+		uint64_t begin_ic = 0;
+		uint64_t bench_time = 0;
+		uint64_t bench_ic = 0;
+		uint64_t first = 0;
+		std::vector<uint64_t> samples;
+	} state;
+	machine.set_userdata(&state);
 
 	// Stop (pause) the machine when he hit a trap/break instruction
 	machine.install_syscall_handler(riscv::SYSCALL_EBREAK,
 	[] (auto& machine) {
-		auto* state = machine.template get_userdata<StartupState>();
-		state->break_used = true;
-		machine.stop();
-		// When we return from this function the internal PC will increment
-		// past this instruction, which allows the Machine to be resumed
-		// just after the break, afterwards.
+		auto* state = machine.template get_userdata<BenchmarkState>();
+		const auto addr = machine.template sysarg<uint64_t>(0);
+
+		if (state->benchmark) throw std::runtime_error("Already benchmarking");
+		state->benchmark = true;
+
+		auto pf = machine.get_printer();
+		machine.set_printer([] (const char*, size_t) {});
+		uint64_t ic = machine.instruction_counter();
+
+		asm("" : : : "memory");
+		const uint64_t t0 = micros_now();
+		asm("" : : : "memory");
+
+		machine.template preempt<0>(addr);
+
+		asm("" : : : "memory");
+		const uint64_t t1 = micros_now();
+		asm("" : : : "memory");
+
+		machine.set_printer(pf);
+		state->benchmark = false;
+		if (state->begin_ic == 0) {
+			state->begin_ic = machine.instruction_counter();
+			state->bench_time = t0;
+			state->bench_ic = machine.instruction_counter() - ic;
+			state->first = t1 - t0;
+		} else {
+			state->samples.push_back(t1 - t0);
+		}
 	});
 
 	// Execute until we have hit a break
@@ -70,66 +101,29 @@ protected_execute(const Request& req, Response& res, const ContentReader& creade
 		res.set_header("X-Exception", e.what());
 	}
 	asm("" : : : "memory");
-	const uint64_t st1 = micros_now();
+	const uint64_t st2 = micros_now();
 	asm("" : : : "memory");
-	res.set_header("X-Startup-Time", std::to_string(st1 - st0));
-	const auto ic = machine.instruction_counter();
-	res.set_header("X-Startup-Instructions", std::to_string(ic));
-	// cache for 10 seconds (it's only the output of a program)
-	res.set_header("Cache-Control", "max-age=10");
+	res.set_header("X-Startup-Time", std::to_string(state.bench_time - st0));
+	res.set_header("X-Total-Time", std::to_string(st2 - st0));
+	res.set_header("X-Startup-Instructions", std::to_string(state.begin_ic));
+	// Cache for 2 seconds (the benchmark results)
+	res.set_header("Cache-Control", "max-age=2");
 
-	if (startup_state.break_used == true)
-	{
-		startup_state.break_used = false;
-		// Reset PC here for benchmarking
-		machine.reset_instruction_counter();
-		std::deque<uint64_t> samples;
-
-		asm("" : : : "memory");
-		const uint64_t t0 = micros_now();
-		asm("" : : : "memory");
-
-		try {
-			machine.simulate(MAX_INSTRUCTIONS);
-		} catch (const std::exception& e) {
-			printf("Exception after break: %s\n", e.what());
-			res.set_header("X-Exception", e.what());
-		}
-
-		asm("" : : : "memory");
-		const uint64_t t1 = micros_now();
-		asm("" : : : "memory");
-		samples.push_back(t1 - t0);
-
-		if (!samples.empty()) {
-			const uint64_t first = samples[0];
-			std::sort(samples.begin(), samples.end());
-			const uint64_t lowest = samples[0];
-			const uint64_t median = samples[samples.size() / 2];
-			const uint64_t highest = samples[samples.size()-1];
-			res.set_header("X-Runtime-First", std::to_string(first));
-			res.set_header("X-Runtime-Lowest", std::to_string(lowest));
-			res.set_header("X-Runtime-Median", std::to_string(median));
-			res.set_header("X-Runtime-Highest", std::to_string(highest));
-		}
-		const auto ic = machine.instruction_counter();
-		res.set_header("X-Instruction-Count", std::to_string(ic));
-
-		// If ebreak was used again to delineate the benchmark,
-		// we can finish the execution of main here without
-		// resetting the instruction counter.
-		if (startup_state.break_used) {
-			try {
-				machine.simulate(MAX_INSTRUCTIONS);
-			} catch (const std::exception& e) {
-				printf("Exception after break: %s\n", e.what());
-				res.set_header("X-Exception", e.what());
-			}
-		}
+	auto& samples = state.samples;
+	if (!samples.empty()) {
+		std::sort(samples.begin(), samples.end());
+		const uint64_t lowest = samples[0];
+		const uint64_t median = samples[samples.size() / 2];
+		const uint64_t highest = samples[samples.size()-1];
+		res.set_header("X-Runtime-Samples", std::to_string(samples.size()));
+		res.set_header("X-Runtime-Lowest", std::to_string(lowest));
+		res.set_header("X-Runtime-Median", std::to_string(median));
+		res.set_header("X-Runtime-Highest", std::to_string(highest));
 	}
-	else {
-		res.set_header("X-Instruction-Count", "0");
-	}
+
+	// Instruction count of first benchmark
+	res.set_header("X-Runtime-First", std::to_string(state.first));
+	res.set_header("X-Instruction-Count", std::to_string(state.bench_ic));
 
 	res.set_header("X-Binary-Size", std::to_string(binary.size()));
 	const size_t active_mem = machine.memory.pages_active() * 4096;
