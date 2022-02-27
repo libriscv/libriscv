9a01f69638d54765ee14cbc0f6c9933211801a43
Alf-André Walla <fwsgonzo@hotmail.com>
2022-02-27 03:13:42 +0100

    Add Catch2, don't init submodules by default

--- setup.sh
+++ setup.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-# sudo apt install npm
-# sudo npm install --global xpm
-# xpm install --global @xpack-dev-tools/riscv-none-embed-gcc@latest
-git submodule update --init
 if [[ -z "${NO_BUILD}" ]]; then
 	pushd emulator
 	source build.sh
