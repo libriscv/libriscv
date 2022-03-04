bace5d445874a87b2633e270c0d869fb180f0cff
Alf-André Walla <fwsgonzo@hotmail.com>
2022-03-04 01:42:48 +0100

    Add helper for T1 register

--- lib/libriscv/riscvbase.hpp
+++ lib/libriscv/riscvbase.hpp
@@ -8,6 +8,7 @@ namespace riscv
 	static const uint32_t REG_GP   = 3;
 	static const uint32_t REG_TP   = 4;
 	static const uint32_t REG_T0   = 5;
+	static const uint32_t REG_T1   = 6;
 	static const uint32_t REG_RETVAL = 10;
 	static const uint32_t REG_ARG0   = 10;
 	static const uint32_t REG_ARG1   = 11;
