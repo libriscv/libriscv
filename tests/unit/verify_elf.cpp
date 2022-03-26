41c6d216c0390b2e16d4f11c7b3435e97eee239a
Alf-André Walla <fwsgonzo@hotmail.com>
2022-03-26 01:58:35 +0100

    Add unit test for Golang hello world

---
+++ tests/unit/verify_elf.cpp
@@ -0,0 +1,40 @@
+#include <catch2/catch_test_macros.hpp>
+#include <libriscv/machine.hpp>
+extern std::vector<uint8_t> load_file(const std::string& filename);
+static const uint64_t MAX_MEMORY = 8ul << 20; /* 8MB */
+static const uint64_t MAX_INSTRUCTIONS = 10'000'000ul;
+static const std::string cwd {SRCDIR};
+using namespace riscv;
+
+TEST_CASE("Golang Hello World", "[Verify]")
+{
+	const auto binary = load_file(cwd + "/elf/golang-riscv64-hello-world");
+
+	riscv::Machine<RISCV64> machine { binary, { .memory_max = MAX_MEMORY } };
+	// We need to install Linux system calls for maximum gucciness
+	machine.setup_linux_syscalls();
+	machine.fds().permit_filesystem = true;
+	machine.fds().permit_sockets = false;
+	machine.fds().filter_open = [] (void* user, const std::string& path) {
+		(void) user; (void) path;
+		return false;
+	};
+	// multi-threading
+	machine.setup_posix_threads();
+	// We need to create a Linux environment for runtimes to work well
+	machine.setup_linux(
+		{"golang-riscv64-hello-world"},
+		{"LC_TYPE=C", "LC_ALL=C", "USER=root"});
+
+	bool output_is_hello_world = false;
+	machine.set_printer([&] (const char* data, size_t size) {
+		std::string text{data, data + size};
+		output_is_hello_world = (text == "hello world");
+	});
+
+	// Run for at most X instructions before giving up
+	machine.simulate(MAX_INSTRUCTIONS);
+
+	REQUIRE(machine.return_value() == 0);
+	REQUIRE(output_is_hello_world);
+}
