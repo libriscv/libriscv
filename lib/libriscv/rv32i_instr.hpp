0d1c212e863c517bed85d9391257080020d8fc67
Alf-André Walla <fwsgonzo@hotmail.com>
2022-04-01 12:07:02 +0200

    Improve the Itype signed IMM calculation

--- lib/libriscv/rv32i_instr.hpp
+++ lib/libriscv/rv32i_instr.hpp
@@ -37,9 +37,8 @@ namespace riscv
 				return imm & 0x800;
 			}
 			int32_t signed_imm() const noexcept {
-				constexpr uint32_t ext = 0xFFFFF000;
-				return imm | (sign() ? ext : 0);
-				//return int32_t(imm << 20) >> 20;
+				// Arithmetic shift for sign-extension
+				return int32_t(imm << 20) >> 20;
 			}
 			uint32_t shift_imm() const noexcept {
 				return imm & 0x1F;
