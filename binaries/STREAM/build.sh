ce4de4a4af53643d3998b6dd79aa1bc019b74f0e
Alf-André Walla <fwsgonzo@hotmail.com>
2022-10-18 14:54:04 +0200

    Make STREAM example support both 32- and 64-bit, and TUNED

--- binaries/STREAM/build.sh
+++ binaries/STREAM/build.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 set -e
 
+GCC_TRIPLE="riscv32-unknown-elf"
+
 RISCV_TC=$HOME/riscv
 export PATH=$PATH:$RISCV_TC/bin
-export CC=$RISCV_TC/bin/riscv64-unknown-elf-gcc
-export CXX=$RISCV_TC/bin/riscv64-unknown-elf-g++
+export CC=$RISCV_TC/bin/$GCC_TRIPLE-gcc
+export CXX=$RISCV_TC/bin/$GCC_TRIPLE-g++
 #export CC=riscv64-linux-gnu-gcc
 #export CXX=riscv64-linux-gnu-g++
 
 mkdir -p build
 pushd build
-cmake .. -DCMAKE_TOOLCHAIN_FILE=toolchain.cmake $@
+cmake .. -DGCC_TRIPLE=$GCC_TRIPLE -DCMAKE_TOOLCHAIN_FILE=toolchain.cmake $@
 make -j4
 popd
