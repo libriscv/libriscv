7a18f8881ee29256c1de825c062267e15890f8ce
Alf-André Walla <fwsgonzo@hotmail.com>
2022-11-11 14:54:43 +0100

    Improve automatic compiler detection for unit tests

--- tests/unit/run_experimentally.sh
+++ tests/unit/run_experimentally.sh
@@ -1,13 +1,6 @@
 FOLDER=build_unittests_exp
 set -e
-export RCC="riscv64-linux-gnu-gcc-10"
-export RCXX="riscv64-linux-gnu-g++-10"
-
-if command -v "riscv64-linux-gnu-gcc-11" &> /dev/null
-then
-    export RCC="riscv64-linux-gnu-gcc-11"
-    export RCXX="riscv64-linux-gnu-g++-11"
-fi
+source scripts/find_compiler.sh
 
 mkdir -p $FOLDER
 pushd $FOLDER
---
+++ tests/unit/scripts/find_compiler.sh
@@ -0,0 +1,18 @@
+if command -v "riscv64-linux-gnu-gcc-12" &> /dev/null
+then
+    echo "Found RISC-V compiler: GCC 12"
+    export RCC="riscv64-linux-gnu-gcc-12"
+    export RCXX="riscv64-linux-gnu-g++-12"
+
+elif command -v "riscv64-linux-gnu-gcc-11" &> /dev/null
+then
+    echo "Found RISC-V compiler: GCC 11"
+    export RCC="riscv64-linux-gnu-gcc-11"
+    export RCXX="riscv64-linux-gnu-g++-11"
+
+elif command -v "riscv64-linux-gnu-gcc-10" &> /dev/null
+then
+    echo "Found RISC-V compiler: GCC 10"
+    export RCC="riscv64-linux-gnu-gcc-10"
+    export RCXX="riscv64-linux-gnu-g++-10"
+fi
