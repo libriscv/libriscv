1ba6e555c95b0491a50219f671f4fc18a54856ae
Alf-André Walla <fwsgonzo@hotmail.com>
2022-11-16 18:15:53 +0100

    Update instruction counter properly during binary translation

--- lib/libriscv/cpu.cpp
+++ lib/libriscv/cpu.cpp
@@ -178,15 +178,33 @@ namespace riscv
 #else // RISCV_FAST_SIMULATOR
 
 	// In fastsim mode the instruction counter becomes a register
-	// the function, and we only m_counter in Machine on exit
+	// the function, and we only update m_counter in Machine on exit
+	// When binary translation is enabled we cannot do this optimization.
 	template <int W>
 	struct InstrCounter {
 		InstrCounter(Machine<W>& m) : machine(m), m_counter{m.instruction_counter()} {}
-		~InstrCounter() { machine.set_instruction_counter(m_counter); }
+		~InstrCounter() {
+			if constexpr (!binary_translation_enabled)
+				machine.set_instruction_counter(m_counter);
+		}
 
-		uint64_t value() const noexcept { return m_counter; }
-		void increment_counter(uint64_t cnt) { m_counter += cnt; }
-		bool overflowed() const noexcept { return m_counter > machine.max_instructions(); }
+		uint64_t value() const noexcept {
+			if constexpr (binary_translation_enabled)
+				return machine.instruction_counter();
+			else
+				return m_counter; }
+		void increment_counter(uint64_t cnt) {
+			if constexpr (binary_translation_enabled)
+				machine.increment_counter(cnt);
+			else
+				m_counter += cnt;
+		}
+		bool overflowed() const noexcept {
+			if constexpr (binary_translation_enabled)
+				return machine.stopped();
+			else
+				return m_counter > machine.max_instructions();
+		}
 	private:
 		Machine<W>& machine;
 		uint64_t m_counter;
