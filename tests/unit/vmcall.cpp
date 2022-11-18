e6f8e494e7553f0055688b113dad25c1272b91fe
Alf-André Walla <fwsgonzo@hotmail.com>
2022-11-18 13:35:15 +0100

    Add forking vmcall test

--- tests/unit/vmcall.cpp
+++ tests/unit/vmcall.cpp
@@ -52,3 +52,53 @@ TEST_CASE("VM function call", "[VMCall]")
 	// Now hello world should have been printed
 	REQUIRE(state.output_is_hello_world);
 }
+
+TEST_CASE("VM function call in fork", "[VMCall]")
+{
+	// The global variable 'value' should get
+	// forked as value=1. We assert this, then
+	// we set value=0. New forks should continue
+	// to see value=1 as they are forked from the
+	// main VM where value is still 0.
+	const auto binary = build_and_load(R"M(
+	#include <assert.h>
+	extern long write(int, const void*, unsigned long);
+	static int value = 0;
+
+	extern void hello() {
+		assert(value == 1);
+		value = 0;
+		write(1, "Hello World!", 12);
+	}
+
+	int main() {
+		value = 1;
+		return 666;
+	})M");
+
+	riscv::Machine<RISCV64> machine { binary, { .memory_max = MAX_MEMORY } };
+	machine.setup_linux_syscalls();
+	machine.setup_linux(
+		{"vmcall"},
+		{"LC_TYPE=C", "LC_ALL=C", "USER=root"});
+
+	machine.simulate(MAX_INSTRUCTIONS);
+	REQUIRE(machine.return_value<int>() == 666);
+
+	// Test many forks
+	for (size_t i = 0; i < 10; i++)
+	{
+		riscv::Machine<RISCV64> fork { machine };
+
+		fork.set_printer([] (const auto&, const char* data, size_t size) {
+			std::string text{data, data + size};
+			REQUIRE(text == "Hello World!");
+		});
+
+		const auto hello_address = fork.address_of("hello");
+		REQUIRE(hello_address != 0x0);
+
+		// Execute guest function
+		fork.vmcall(hello_address);
+	}
+}
