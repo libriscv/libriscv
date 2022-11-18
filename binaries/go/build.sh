e69017b24fec00d2e7d95f8001271e40cac91567
Alf-André Walla <fwsgonzo@hotmail.com>
2022-11-18 15:36:53 +0100

    Build the Go fib example

--- binaries/go/build.sh
+++ binaries/go/build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 GOARCH=riscv64 go build channel_ops.go
+GOARCH=riscv64 go build fib.go
 GOARCH=riscv64 go build example.go
 GOARCH=riscv64 go build goroutines.go
 GOARCH=riscv64 go build http_client.go
