8ad73ec20c4fdb98bae17336f714daed848e062f
Alf-André Walla <fwsgonzo@hotmail.com>
2022-11-18 15:36:06 +0100

    Make fib a freestanding program

---
+++ binaries/measure_mips/build32.sh
@@ -0,0 +1,1 @@
+riscv32-unknown-elf-gcc -O2 -static -ffreestanding -nostdlib fib.c -o fib
--- binaries/measure_mips/fib.c
+++ binaries/measure_mips/fib.c
@@ -1,13 +1,22 @@
 static long fib(long n, long acc, long prev)
 {
-    if (n == 0)
-        return acc;
-    else
-        return fib(n - 1, prev + acc, acc);
+	if (n == 0)
+		return acc;
+	else
+		return fib(n - 1, prev + acc, acc);
 }
 
-int main(int argc, char** argv)
+inline long syscall(long n, long arg0) {
+	register long a0 asm("a0") = arg0;
+	register long syscall_id asm("a7") = n;
+
+	__asm__ volatile ("scall" : "+r"(a0) : "r"(syscall_id));
+
+	return a0;
+}
+
+void _start()
 {
-    const volatile long n = 256000000;
-    return fib(n, 0, 1);
+	const volatile long n = 256000000;
+	syscall(93, fib(n, 0, 1));
 }
