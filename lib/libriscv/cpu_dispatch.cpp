a3b5965de5081114479e3b3621bb931bb580b94f
Alf-André Walla <fwsgonzo@hotmail.com>
2022-12-04 13:05:55 +0100

    Make jump labels macro-based in order to support switch case in future

--- lib/libriscv/cpu_dispatch.cpp
+++ lib/libriscv/cpu_dispatch.cpp
@@ -180,38 +180,40 @@ continue_block:
 
 	goto *computed_opcode[decoder->get_bytecode()];
 
+#define INSTRUCTION(bc, lbl) lbl
+
 #ifdef RISCV_EXT_COMPRESSED
-rv32c_addi: {
+INSTRUCTION(RV32C_BC_ADDI, rv32c_addi): {
 	VIEW_INSTR_AS(fi, FasterItype);
 	this->reg(fi.rs1) = reg(fi.rs2) + fi.signed_imm();
 	NEXT_C_INSTR();
 }
 #endif
-rv32i_addi: {
+INSTRUCTION(RV32I_BC_ADDI, rv32i_addi): {
 	VIEW_INSTR_AS(fi, FasterItype);
 	this->reg(fi.rs1) =
 		this->reg(fi.rs2) + fi.signed_imm();
 	NEXT_INSTR();
 }
-rv32i_li: {
+INSTRUCTION(RV32I_BC_LI, rv32i_li): {
 	VIEW_INSTR_AS(fi, FasterItype);
 	this->reg(fi.rs1) = fi.signed_imm();
 	NEXT_INSTR();
 }
-rv32i_ldw: {
+INSTRUCTION(RV32I_BC_LDW, rv32i_ldw): {
 	VIEW_INSTR_AS(fi, FasterItype);
 	const auto addr = this->reg(fi.rs2) + fi.signed_imm();
 	this->reg(fi.rs1) =
 		(int32_t)machine().memory.template read<uint32_t>(addr);
 	NEXT_INSTR();
 }
-rv32i_stw: {
+INSTRUCTION(RV32I_BC_STW, rv32i_stw): {
 	VIEW_INSTR_AS(fi, FasterItype);
 	const auto addr  = reg(fi.rs1) + fi.signed_imm();
 	machine().memory.template write<uint32_t>(addr, reg(fi.rs2));
 	NEXT_INSTR();
 }
-rv32i_ldwu: {
+INSTRUCTION(RV32I_BC_LDWU, rv32i_ldwu): {
 	if constexpr (W >= 8) {
 		VIEW_INSTR_AS(fi, FasterItype);
 		const auto addr = this->reg(fi.rs2) + fi.signed_imm();
@@ -220,7 +222,7 @@ rv32i_ldwu: {
 		NEXT_INSTR();
 	}
 }
-rv32i_ldd: {
+INSTRUCTION(RV32I_BC_LDD, rv32i_ldd): {
 	if constexpr (W >= 8) {
 		VIEW_INSTR_AS(fi, FasterItype);
 		const auto addr = this->reg(fi.rs2) + fi.signed_imm();
@@ -229,7 +231,7 @@ rv32i_ldd: {
 		NEXT_INSTR();
 	}
 }
-rv32i_std: {
+INSTRUCTION(RV32I_BC_STD, rv32i_std): {
 	if constexpr (W >= 8) {
 		VIEW_INSTR_AS(fi, FasterItype);
 		const auto addr  = reg(fi.rs1) + fi.signed_imm();
@@ -237,49 +239,49 @@ rv32i_std: {
 		NEXT_INSTR();
 	}
 }
-rv32i_beq: {
+INSTRUCTION(RV32I_BC_BEQ, rv32i_beq): {
 	VIEW_INSTR_AS(fi, FasterItype);
 	if (reg(fi.rs1) == reg(fi.rs2)) {
 		PERFORM_FAST_BRANCH();
 	}
 	NEXT_BLOCK(4);
 }
-rv32i_bne: {
+INSTRUCTION(RV32I_BC_BNE, rv32i_bne): {
 	VIEW_INSTR_AS(fi, FasterItype);
 	if (reg(fi.rs1) != reg(fi.rs2)) {
 		PERFORM_FAST_BRANCH();
 	}
 	NEXT_BLOCK(4);
 }
-rv32i_blt: {
+INSTRUCTION(RV32I_BC_BLT, rv32i_blt): {
 	VIEW_INSTR_AS(fi, FasterItype);
 	if ((saddr_t)reg(fi.rs1) < (saddr_t)reg(fi.rs2)) {
 		PERFORM_FAST_BRANCH();
 	}
 	NEXT_BLOCK(4);
 }
-rv32i_bge: {
+INSTRUCTION(RV32I_BC_BGE, rv32i_bge): {
 	VIEW_INSTR_AS(fi, FasterItype);
 	if ((saddr_t)reg(fi.rs1) >= (saddr_t)reg(fi.rs2)) {
 		PERFORM_FAST_BRANCH();
 	}
 	NEXT_BLOCK(4);
 }
-rv32i_bltu: {
+INSTRUCTION(RV32I_BC_BLTU, rv32i_bltu): {
 	VIEW_INSTR_AS(fi, FasterItype);
 	if (reg(fi.rs1) < reg(fi.rs2)) {
 		PERFORM_FAST_BRANCH();
 	}
 	NEXT_BLOCK(4);
 }
-rv32i_bgeu: {
+INSTRUCTION(RV32I_BC_BGEU, rv32i_bgeu): {
 	VIEW_INSTR_AS(fi, FasterItype);
 	if (reg(fi.rs1) >= reg(fi.rs2)) {
 		PERFORM_FAST_BRANCH();
 	}
 	NEXT_BLOCK(4);
 }
-rv32i_op_add: {
+INSTRUCTION(RV32I_BC_OP_ADD, rv32i_op_add): {
 	#define OP_INSTR() \
 		VIEW_INSTR_AS(fi, FasterOpType); \
 		auto& dst = reg(fi.rd); \
@@ -289,73 +291,73 @@ rv32i_op_add: {
 	dst = src1 + src2;
 	NEXT_INSTR();
 }
-rv32i_op_sub: {
+INSTRUCTION(RV32I_BC_OP_SUB, rv32i_op_sub): {
 	OP_INSTR();
 	dst = src1 - src2;
 	NEXT_INSTR();
 }
-rv32i_slli: {
+INSTRUCTION(RV32I_BC_SLLI, rv32i_slli): {
 	VIEW_INSTR_AS(fi, FasterItype);
 	// SLLI: Logical left-shift 5/6/7-bit immediate
 	this->reg(fi.rs1) =
 		this->reg(fi.rs2) << (fi.unsigned_imm() & (XLEN - 1));
 	NEXT_INSTR();
 }
-rv32i_slti: {
+INSTRUCTION(RV32I_BC_SLTI, rv32i_slti): {
 	VIEW_INSTR_AS(fi, FasterItype);
 	// SLTI: Set less than immediate
 	this->reg(fi.rs1) = (saddr_t(reg(fi.rs2)) < fi.signed_imm());
 	NEXT_INSTR();
 }
-rv32i_sltiu: {
+INSTRUCTION(RV32I_BC_SLTIU, rv32i_sltiu): {
 	VIEW_INSTR_AS(fi, FasterItype);
 	// SLTIU: Sign-extend, then treat as unsigned
 	this->reg(fi.rs1) = (reg(fi.rs2) < addr_t(fi.signed_imm()));
 	NEXT_INSTR();
 }
-rv32i_xori: {
+INSTRUCTION(RV32I_BC_XORI, rv32i_xori): {
 	VIEW_INSTR_AS(fi, FasterItype);
 	// XORI
 	this->reg(fi.rs1) = reg(fi.rs2) ^ fi.signed_imm();
 	NEXT_INSTR();
 }
-rv32i_srli: {
+INSTRUCTION(RV32I_BC_SRLI, rv32i_srli): {
 	VIEW_INSTR_AS(fi, FasterItype);
 	// SRLI: Shift-right logical 5/6/7-bit immediate
 	this->reg(fi.rs1) = reg(fi.rs2) >> (fi.unsigned_imm() & (XLEN - 1));
 	NEXT_INSTR();
 }
-rv32i_srai: {
+INSTRUCTION(RV32I_BC_SRAI, rv32i_srai): {
 	VIEW_INSTR_AS(fi, FasterItype);
 	// SRAI: Shift-right arithmetical (preserve the sign bit)
 	this->reg(fi.rs1) = saddr_t(reg(fi.rs2)) >> (fi.unsigned_imm() & (XLEN - 1));
 	NEXT_INSTR();
 }
-rv32i_ori: {
+INSTRUCTION(RV32I_BC_ORI, rv32i_ori): {
 	VIEW_INSTR_AS(fi, FasterItype);
 	// ORI: Or sign-extended 12-bit immediate
 	this->reg(fi.rs1) = reg(fi.rs2) | fi.signed_imm();
 	NEXT_INSTR();
 }
-rv32i_andi: {
+INSTRUCTION(RV32I_BC_ANDI, rv32i_andi): {
 	VIEW_INSTR_AS(fi, FasterItype);
 	// ANDI: And sign-extended 12-bit immediate
 	this->reg(fi.rs1) = reg(fi.rs2) & fi.signed_imm();
 	NEXT_INSTR();
 }
-rv32i_jal: {
+INSTRUCTION(RV32I_BC_JAL, rv32i_jal): {
 	VIEW_INSTR_AS(fi, FasterJtype);
 	if (fi.rd != 0)
 		reg(fi.rd) = pc + 4;
 	pc += fi.offset;
 	goto check_jump;
 }
-rv32i_fast_jal: {
+INSTRUCTION(RV32I_BC_FAST_JAL, rv32i_fast_jal): {
 	VIEW_INSTR();
 	pc = instr.whole;
 	goto check_jump;
 }
-rv32i_jalr: {
+INSTRUCTION(RV32I_BC_JALR, rv32i_jalr): {
 	VIEW_INSTR();
 	// jump to register + immediate
 	// NOTE: if rs1 == rd, avoid clobber by storing address first
@@ -367,20 +369,20 @@ rv32i_jalr: {
 	pc = address;
 	goto check_unaligned_jump;
 }
-rv32i_lui: {
+INSTRUCTION(RV32I_BC_LUI, rv32i_lui): {
 	VIEW_INSTR();
 	this->reg(instr.Utype.rd) = instr.Utype.upper_imm();
 	NEXT_INSTR();
 }
 
 #ifdef RISCV_EXT_COMPRESSED
-rv32c_func: {
+INSTRUCTION(RV32C_BC_FUNCTION, rv32c_func): {
 	VIEW_INSTR();
 	auto handler = decoder->get_handler();
 	handler(*this, instr);
 	NEXT_C_INSTR();
 }
-rv32c_jfunc: {
+INSTRUCTION(RV32C_BC_JUMPFUNC, rv32c_jfunc): {
 	VIEW_INSTR();
 	registers().pc = pc;
 	auto handler = decoder->get_handler();
@@ -390,47 +392,47 @@ rv32c_jfunc: {
 }
 #endif
 
-rv32i_op_sll: {
+INSTRUCTION(RV32I_BC_OP_SLL, rv32i_op_sll): {
 	OP_INSTR();
 	dst = src1 << (src2 & (XLEN - 1));
 	NEXT_INSTR();
 }
-rv32i_op_slt: {
+INSTRUCTION(RV32I_BC_OP_SLT, rv32i_op_slt): {
 	OP_INSTR();
 	dst = (saddr_t(src1) < saddr_t(src2));
 	NEXT_INSTR();
 }
-rv32i_op_sltu: {
+INSTRUCTION(RV32I_BC_OP_SLTU, rv32i_op_sltu): {
 	OP_INSTR();
 	dst = (src1 < src2);
 	NEXT_INSTR();
 }
-rv32i_op_xor: {
+INSTRUCTION(RV32I_BC_OP_XOR, rv32i_op_xor): {
 	OP_INSTR();
 	dst = src1 ^ src2;
 	NEXT_INSTR();
 }
-rv32i_op_srl: {
+INSTRUCTION(RV32I_BC_OP_SRL, rv32i_op_srl): {
 	OP_INSTR();
 	dst = src1 >> (src2 & (XLEN - 1));
 	NEXT_INSTR();
 }
-rv32i_op_or: {
+INSTRUCTION(RV32I_BC_OP_OR, rv32i_op_or): {
 	OP_INSTR();
 	dst = src1 | src2;
 	NEXT_INSTR();
 }
-rv32i_op_and: {
+INSTRUCTION(RV32I_BC_OP_AND, rv32i_op_and): {
 	OP_INSTR();
 	dst = src1 & src2;
 	NEXT_INSTR();
 }
-rv32i_op_mul: {
+INSTRUCTION(RV32I_BC_OP_MUL, rv32i_op_mul): {
 	OP_INSTR();
 	dst = saddr_t(src1) * saddr_t(src2);
 	NEXT_INSTR();
 }
-rv32i_op_div: {
+INSTRUCTION(RV32I_BC_OP_DIV, rv32i_op_div): {
 	OP_INSTR();
 	// division by zero is not an exception
 	if (LIKELY(saddr_t(src2) != 0)) {
@@ -450,23 +452,23 @@ rv32i_op_div: {
 	}
 	NEXT_INSTR();
 }
-rv32i_op_sh1add: {
+INSTRUCTION(RV32I_BC_OP_SH1ADD, rv32i_op_sh1add): {
 	OP_INSTR();
 	dst = src2 + (src1 << 1);
 	NEXT_INSTR();
 }
-rv32i_op_sh2add: {
+INSTRUCTION(RV32I_BC_OP_SH2ADD, rv32i_op_sh2add): {
 	OP_INSTR();
 	dst = src2 + (src1 << 2);
 	NEXT_INSTR();
 }
-rv32i_op_sh3add: {
+INSTRUCTION(RV32I_BC_OP_SH3ADD, rv32i_op_sh3add): {
 	OP_INSTR();
 	dst = src2 + (src1 << 3);
 	NEXT_INSTR();
 }
 
-rv32i_syscall: {
+INSTRUCTION(RV32I_BC_SYSCALL, rv32i_syscall): {
 	// Make the current PC visible
 	this->registers().pc = pc;
 	// Make the instruction counter visible
@@ -484,76 +486,76 @@ rv32i_syscall: {
 	NEXT_BLOCK(4);
 }
 
-rv32i_ldb: {
+INSTRUCTION(RV32I_BC_LDB, rv32i_ldb): {
 	VIEW_INSTR_AS(fi, FasterItype);
 	const auto addr = this->reg(fi.rs2) + fi.signed_imm();
 	this->reg(fi.rs1) =
 		int8_t(machine().memory.template read<uint8_t>(addr));
 	NEXT_INSTR();
 }
-rv32i_ldbu: {
+INSTRUCTION(RV32I_BC_LDBU, rv32i_ldbu): {
 	VIEW_INSTR_AS(fi, FasterItype);
 	const auto addr = this->reg(fi.rs2) + fi.signed_imm();
 	this->reg(fi.rs1) =
 		saddr_t(machine().memory.template read<uint8_t>(addr));
 	NEXT_INSTR();
 }
-rv32i_ldh: {
+INSTRUCTION(RV32I_BC_LDH, rv32i_ldh): {
 	VIEW_INSTR_AS(fi, FasterItype);
 	const auto addr = this->reg(fi.rs2) + fi.signed_imm();
 	this->reg(fi.rs1) =
 		int16_t(machine().memory.template read<uint16_t>(addr));
 	NEXT_INSTR();
 }
-rv32i_ldhu: {
+INSTRUCTION(RV32I_BC_LDHU, rv32i_ldhu): {
 	VIEW_INSTR_AS(fi, FasterItype);
 	const auto addr = this->reg(fi.rs2) + fi.signed_imm();
 	this->reg(fi.rs1) =
 		saddr_t(machine().memory.template read<uint16_t>(addr));
 	NEXT_INSTR();
 }
-rv32i_stb: {
+INSTRUCTION(RV32I_BC_STB, rv32i_stb): {
 	VIEW_INSTR_AS(fi, FasterItype);
 	const auto addr = reg(fi.rs1) + fi.signed_imm();
 	machine().memory.template write<uint8_t>(addr, reg(fi.rs2));
 	NEXT_INSTR();
 }
-rv32i_sth: {
+INSTRUCTION(RV32I_BC_STH, rv32i_sth): {
 	VIEW_INSTR_AS(fi, FasterItype);
 	const auto addr = reg(fi.rs1) + fi.signed_imm();
 	machine().memory.template write<uint16_t>(addr, reg(fi.rs2));
 	NEXT_INSTR();
 }
 
-rv32i_flw: {
+INSTRUCTION(RV32F_BC_FLW, rv32i_flw): {
 	VIEW_INSTR_AS(fi, rv32f_instruction);
 	auto addr = reg(fi.Itype.rs1) + fi.Itype.signed_imm();
 	auto& dst = registers().getfl(fi.Itype.rd);
 	dst.load_u32(machine().memory.template read<uint32_t> (addr));
 	NEXT_INSTR();
 }
-rv32i_fld: {
+INSTRUCTION(RV32F_BC_FLD, rv32i_fld): {
 	VIEW_INSTR_AS(fi, rv32f_instruction);
 	auto addr = reg(fi.Itype.rs1) + fi.Itype.signed_imm();
 	auto& dst = registers().getfl(fi.Itype.rd);
 	dst.load_u64(machine().memory.template read<uint64_t> (addr));
 	NEXT_INSTR();
 }
-rv32i_fsw: {
+INSTRUCTION(RV32F_BC_FSW, rv32i_fsw): {
 	VIEW_INSTR_AS(fi, rv32f_instruction);
 	const auto& src = registers().getfl(fi.Stype.rs2);
 	auto addr = reg(fi.Stype.rs1) + fi.Stype.signed_imm();
 	machine().memory.template write<uint32_t> (addr, src.i32[0]);
 	NEXT_INSTR();
 }
-rv32i_fsd: {
+INSTRUCTION(RV32F_BC_FSD, rv32i_fsd): {
 	VIEW_INSTR_AS(fi, rv32f_instruction);
 	const auto& src = registers().getfl(fi.Stype.rs2);
 	auto addr = reg(fi.Stype.rs1) + fi.Stype.signed_imm();
 	machine().memory.template write<uint64_t> (addr, src.i64);
 	NEXT_INSTR();
 }
-rv32i_system: {
+INSTRUCTION(RV32I_BC_SYSTEM, rv32i_system): {
 	VIEW_INSTR();
 	machine().system(instr);
 	// Check if machine stopped
@@ -564,7 +566,7 @@ rv32i_system: {
 	}
 	NEXT_BLOCK(4);
 }
-rv32f_fadd: {
+INSTRUCTION(RV32F_BC_FADD, rv32f_fadd): {
 	VIEW_INSTR_AS(fi, rv32f_instruction);
 	#define FLREGS() \
 		auto& dst = registers().getfl(fi.R4type.rd); \
@@ -581,7 +583,7 @@ rv32f_fadd: {
 	}
 	NEXT_INSTR();
 }
-rv32f_fsub: {
+INSTRUCTION(RV32F_BC_FSUB, rv32f_fsub): {
 	VIEW_INSTR_AS(fi, rv32f_instruction);
 	FLREGS();
 	if (fi.R4type.funct2 == 0x0)
@@ -594,7 +596,7 @@ rv32f_fsub: {
 	}
 	NEXT_INSTR();
 }
-rv32f_fmul: {
+INSTRUCTION(RV32F_BC_FMUL, rv32f_fmul): {
 	VIEW_INSTR_AS(fi, rv32f_instruction);
 	FLREGS();
 	if (fi.R4type.funct2 == 0x0)
@@ -607,7 +609,7 @@ rv32f_fmul: {
 	}
 	NEXT_INSTR();
 }
-rv32f_fdiv: {
+INSTRUCTION(RV32F_BC_FDIV, rv32f_fdiv): {
 	VIEW_INSTR_AS(fi, rv32f_instruction);
 	FLREGS();
 	if (fi.R4type.funct2 == 0x0)
@@ -621,7 +623,7 @@ rv32f_fdiv: {
 	NEXT_INSTR();
 }
 
-check_jump:
+check_jump: {
 	if (UNLIKELY(counter.overflowed())) {
 		registers().pc = pc;
 		return;
@@ -639,31 +641,32 @@ check_jump:
 		goto restart_sim;
 	}
 	goto continue_segment;
+}
 
 /** UNLIKELY INSTRUCTIONS **/
 /** UNLIKELY INSTRUCTIONS **/
 
-execute_decoded_function: {
+INSTRUCTION(RV32I_BC_FUNCTION, execute_decoded_function): {
 	VIEW_INSTR();
 	auto handler = decoder->get_handler();
 	handler(*this, instr);
 	NEXT_INSTR();
 }
 
-rv32i_nop: {
+INSTRUCTION(RV32I_BC_NOP, rv32i_nop): {
 	NEXT_INSTR();
 }
-rv32i_auipc: {
+INSTRUCTION(RV32I_BC_AUIPC, rv32i_auipc): {
 	VIEW_INSTR();
 	this->reg(instr.Utype.rd) = pc + instr.Utype.upper_imm();
 	NEXT_BLOCK(4);
 }
-rv32i_op_sra: {
+INSTRUCTION(RV32I_BC_OP_SRA, rv32i_op_sra): {
 	OP_INSTR();
 	dst = saddr_t(src1) >> (src2 & (XLEN-1));
 	NEXT_INSTR();
 }
-rv32i_op_mulh: {
+INSTRUCTION(RV32I_BC_OP_MULH, rv32i_op_mulh): {
 	OP_INSTR();
 	if constexpr (W == 4) {
 		dst = uint64_t((int64_t)saddr_t(src1) * (int64_t)saddr_t(src2)) >> 32u;
@@ -674,7 +677,7 @@ rv32i_op_mulh: {
 	}
 	NEXT_INSTR();
 }
-rv32i_op_mulhsu: {
+INSTRUCTION(RV32I_BC_OP_MULHSU, rv32i_op_mulhsu): {
 	OP_INSTR();
 	if constexpr (W == 4) {
 		dst = uint64_t((int64_t)saddr_t(src1) * (uint64_t)src2) >> 32u;
@@ -685,7 +688,7 @@ rv32i_op_mulhsu: {
 	}
 	NEXT_INSTR();
 }
-rv32i_op_mulhu: {
+INSTRUCTION(RV32I_BC_OP_MULHU, rv32i_op_mulhu): {
 	OP_INSTR();
 	if constexpr (W == 4) {
 		dst = uint64_t((uint64_t)src1 * (uint64_t)src2) >> 32u;
@@ -696,7 +699,7 @@ rv32i_op_mulhu: {
 	}
 	NEXT_INSTR();
 }
-rv32i_op_divu: {
+INSTRUCTION(RV32I_BC_OP_DIVU, rv32i_op_divu): {
 	OP_INSTR();
 	if (LIKELY(src2 != 0)) {
 		dst = src1 / src2;
@@ -705,7 +708,7 @@ rv32i_op_divu: {
 	}
 	NEXT_INSTR();
 }
-rv32i_op_rem: {
+INSTRUCTION(RV32I_BC_OP_REM, rv32i_op_rem): {
 	OP_INSTR();
 	if (LIKELY(src2 != 0)) {
 		if constexpr(W == 4) {
@@ -720,7 +723,7 @@ rv32i_op_rem: {
 	}
 	NEXT_INSTR();
 }
-rv32i_op_remu: {
+INSTRUCTION(RV32I_BC_OP_REMU, rv32i_op_remu): {
 	OP_INSTR();
 	if (LIKELY(src2 != 0)) {
 		dst = src1 % src2;
@@ -730,14 +733,14 @@ rv32i_op_remu: {
 	NEXT_INSTR();
 }
 #ifdef RISCV_EXT_VECTOR
-rv32v_vle32: {
+INSTRUCTION(RV32V_BC_VLE32, rv32v_vle32): {
 	VIEW_INSTR_AS(vi, rv32v_instruction);
 	const auto addr = reg(vi.VLS.rs1) & ~address_t(VectorLane::size()-1);
 	registers().rvv().get(vi.VLS.vd) =
 		machine().memory.template read<VectorLane> (addr);
 	NEXT_INSTR();
 }
-rv32v_vse32: {
+INSTRUCTION(RV32V_BC_VSE32, rv32v_vse32): {
 	VIEW_INSTR_AS(vi, rv32v_instruction);
 	const auto addr = reg(vi.VLS.rs1) & ~address_t(VectorLane::size()-1);
 	auto& dst = registers().rvv().get(vi.VLS.vd);
@@ -750,7 +753,7 @@ rv32v_vse32: {
 /** UNLIKELY INSTRUCTIONS **/
 
 #ifdef RISCV_BINARY_TRANSLATION
-translated_function: {
+INSTRUCTION(RV32I_BC_TRANSLATOR, translated_function): {
 	VIEW_INSTR();
 	counter.apply();
 	auto handler = decoder->get_handler();
