f451ca7391ba24458a8b7b914741c348c147f361
Alf-André Walla <fwsgonzo@hotmail.com>
2022-12-05 12:47:56 +0100

    Revert "Process blocks in-place"
    This reverts commit dec3fdc30e054c04af3d5990d3ed1a25553bc18e.
    
    It was not obviously faster and takes up more instruction space.

--- lib/libriscv/cpu_dispatch.cpp
+++ lib/libriscv/cpu_dispatch.cpp
@@ -18,25 +18,6 @@ namespace riscv
 	auto instr = decoder->view_instr();
 #define VIEW_INSTR_AS(name, x) \
 	auto name = decoder->template view_instr<x>();
-#define PROCESS_BLOCK()                                  \
-	if constexpr (compressed_enabled)                    \
-	{                                                    \
-		pc += decoder->idxend * 2;                       \
-		counter.increment_counter(decoder->instr_count); \
-	}                                                    \
-	else                                                 \
-	{                                                    \
-		unsigned count = decoder->idxend;                \
-		pc += count * 4;                                 \
-		counter.increment_counter(count + 1);            \
-	}
-
-#define PERFORM_BRANCH()            \
-	pc += instr.Btype.signed_imm(); \
-	goto check_jump;
-#define PERFORM_FAST_BRANCH() \
-	pc += fi.signed_imm();    \
-	goto check_jump;
 
 #ifdef DISPATCH_MODE_SWITCH_BASED
 
@@ -49,14 +30,6 @@ namespace riscv
 	break;
 #define NEXT_C_INSTR()                \
 	decoder += 1;                     \
-	break;
-#define NEXT_BLOCK(len)               \
-	pc += len;                        \
-	if constexpr (compressed_enabled) \
-		decoder += 2;                 \
-	else                              \
-		decoder += 1;                 \
-	PROCESS_BLOCK();                  \
 	break;
 
 #else
@@ -71,16 +44,22 @@ namespace riscv
 #define NEXT_C_INSTR() \
 	decoder += 1;      \
 	goto *computed_opcode[decoder->get_bytecode()];
+
+#endif // Dispatch mode
+
 #define NEXT_BLOCK(len)               \
 	pc += len;                        \
 	if constexpr (compressed_enabled) \
 		decoder += 2;                 \
 	else                              \
 		decoder += 1;                 \
-	PROCESS_BLOCK();                  \
-	goto *computed_opcode[decoder->get_bytecode()];
-
-#endif // Dispatch mode
+	goto continue_block;
+#define PERFORM_BRANCH()            \
+	pc += instr.Btype.signed_imm(); \
+	goto check_jump;
+#define PERFORM_FAST_BRANCH() \
+	pc += fi.signed_imm(); \
+	goto check_jump;
 
 template <int W> __attribute__((hot))
 void CPU<W>::DISPATCH_FUNC(uint64_t imax)
@@ -212,8 +191,15 @@ restart_sim:
 continue_segment:
 	decoder = &exec_decoder[pc / DecoderCache<W>::DIVISOR];
 
-	// Process the first block and start
-	PROCESS_BLOCK();
+continue_block:
+	if constexpr (compressed_enabled) {
+		pc += decoder->idxend * 2;
+		counter.increment_counter(decoder->instr_count);
+	} else {
+		unsigned count = decoder->idxend;
+		pc += count * 4;
+		counter.increment_counter(count + 1);
+	}
 
 #ifdef DISPATCH_MODE_SWITCH_BASED
 
@@ -825,19 +811,6 @@ INSTRUCTION(RV32I_BC_TRANSLATOR, translated_function): {
 		__builtin_unreachable();
 #endif
 
-check_unaligned_jump:
-	if constexpr (!compressed_enabled) {
-		if (UNLIKELY(pc & 0x3)) {
-			registers().pc = pc;
-			trigger_exception(MISALIGNED_INSTRUCTION, this->pc());
-		}
-	} else {
-		if (UNLIKELY(pc & 0x1)) {
-			registers().pc = pc;
-			trigger_exception(MISALIGNED_INSTRUCTION, this->pc());
-		}
-	}
-
 check_jump: {
 	if (UNLIKELY(counter.overflowed())) {
 		registers().pc = pc;
@@ -857,6 +830,20 @@ check_jump: {
 	}
 	goto continue_segment;
 }
+check_unaligned_jump:
+	if constexpr (!compressed_enabled) {
+		if (UNLIKELY(pc & 0x3)) {
+			registers().pc = pc;
+			trigger_exception(MISALIGNED_INSTRUCTION, this->pc());
+		}
+	} else {
+		if (UNLIKELY(pc & 0x1)) {
+			registers().pc = pc;
+			trigger_exception(MISALIGNED_INSTRUCTION, this->pc());
+		}
+	}
+	goto check_jump;
+
 } // CPU::simulate_XXX()
 
 } // riscv
