ba35ba5c7a3c7dea7780885b1611a28d667744bc
Alf-André Walla <fwsgonzo@hotmail.com>
2022-12-25 22:15:04 +0100

    Enable threaded by default on Clang and GCC

--- tests/unit/run_unit_tests.sh
+++ tests/unit/run_unit_tests.sh
@@ -4,7 +4,7 @@ source scripts/find_compiler.sh
 
 mkdir -p $FOLDER
 pushd $FOLDER
-cmake .. -DCMAKE_BUILD_TYPE=Debug -DRISCV_MEMORY_TRAPS=ON
+cmake .. -DCMAKE_BUILD_TYPE=Debug -DRISCV_MEMORY_TRAPS=ON -DRISCV_THREADED=OFF
 make -j4
 ctest --verbose . $@
 popd
