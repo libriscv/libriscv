2fa997156be042441274cc5f402eb5d2c34fe7ab
Alf-André Walla <fwsgonzo@hotmail.com>
2022-12-25 17:18:58 +0100

    Add 64-bit Rust hello world test

--- tests/unit/verify_elf.cpp
+++ tests/unit/verify_elf.cpp
@@ -70,3 +70,32 @@ TEST_CASE("Zig Hello World", "[Verify]")
 	REQUIRE(machine.return_value() == 0);
 	REQUIRE(state.text == "Hello, world!\n");
 }
+
+TEST_CASE("Rust Hello World", "[Verify]")
+{
+	const auto binary = load_file(cwd + "/elf/rust-riscv64-hello-world");
+
+	riscv::Machine<RISCV64> machine { binary, { .memory_max = MAX_MEMORY } };
+	// Install Linux system calls
+	machine.setup_linux_syscalls();
+	machine.setup_posix_threads();
+	// Create a Linux environment for runtimes to work well
+	machine.setup_linux(
+		{"rust-riscv64-hello-world"},
+		{"LC_TYPE=C", "LC_ALL=C", "USER=root"});
+
+	struct State {
+		std::string text;
+	} state;
+	machine.set_userdata(&state);
+	machine.set_printer([] (const auto& m, const char* data, size_t size) {
+		auto* state = m.template get_userdata<State> ();
+		state->text.append(data, data + size);
+	});
+
+	// Run for at most X instructions before giving up
+	machine.simulate(MAX_INSTRUCTIONS);
+
+	REQUIRE(machine.return_value() == 0);
+	REQUIRE(state.text == "Hello World!\n");
+}
