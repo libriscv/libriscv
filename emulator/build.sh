ce3bd6b1e3c621179ae9049c5ab6813a51a1e70e
Alf-André Walla <fwsgonzo@hotmail.com>
2023-10-08 13:48:44 +0200

    Hide build folder, it interferes with build script tab completion

--- emulator/build.sh
+++ emulator/build.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
-mkdir -p build
-pushd build
+mkdir -p .build
+pushd .build
 cmake .. -DCMAKE_BUILD_TYPE=Release
 make -j6
 popd
 
-ln -fs build/rvmicro .
-ln -fs build/rvnewlib .
-ln -fs build/rvlinux .
+ln -fs .build/rvmicro .
+ln -fs .build/rvnewlib .
+ln -fs .build/rvlinux .
