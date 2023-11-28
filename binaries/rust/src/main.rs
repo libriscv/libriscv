e998150789d62ca7e7234292c758bb364ffeeb55
Alf-André Walla <fwsgonzo@hotmail.com>
2023-11-28 13:36:24 +0100

    Rust HTTPS example

--- binaries/rust/src/main.rs
+++ binaries/rust/src/main.rs
@@ -7,35 +7,35 @@
 /// runtime is also bound loglinear by the upper term.
 ///
 pub fn nth_prime(n: u32) -> Option<u64> {
-    if n < 1 {
-        return None;
-    }
+	if n < 1 {
+		return None;
+	}
 
-    // The prime counting function is pi(x) which is approximately x/ln(x)
-    // A good upper bound for the nth prime is ceil(x * ln(x * ln(x)))
-    let x = if n <= 10 { 10.0 } else { n as f64 };
-    let limit: usize = (x * (x * (x).ln()).ln()).ceil() as usize;
-    let mut sieve = vec![true; limit];
-    let mut count = 0;
+	// The prime counting function is pi(x) which is approximately x/ln(x)
+	// A good upper bound for the nth prime is ceil(x * ln(x * ln(x)))
+	let x = if n <= 10 { 10.0 } else { n as f64 };
+	let limit: usize = (x * (x * (x).ln()).ln()).ceil() as usize;
+	let mut sieve = vec![true; limit];
+	let mut count = 0;
 
-    // Exceptional case for 0 and 1
-    sieve[0] = false;
-    sieve[1] = false;
+	// Exceptional case for 0 and 1
+	sieve[0] = false;
+	sieve[1] = false;
 
-    for prime in 2..limit {
-        if !sieve[prime] {
-            continue;
-        }
-        count += 1;
-        if count == n {
-            return Some(prime as u64);
-        }
+	for prime in 2..limit {
+		if !sieve[prime] {
+			continue;
+		}
+		count += 1;
+		if count == n {
+			return Some(prime as u64);
+		}
 
-        for multiple in ((prime * prime)..limit).step_by(prime) {
-            sieve[multiple] = false;
-        }
-    }
-    None
+		for multiple in ((prime * prime)..limit).step_by(prime) {
+			sieve[multiple] = false;
+		}
+	}
+	None
 }
 
 fn main() {
@@ -45,4 +45,7 @@ fn main() {
 	assert_eq!(nth_prime(3), Some(5));
 
 	println!("Sieve(256000) == {}\n", nth_prime(256000).unwrap());
+
+	let resp = reqwest::blocking::get("https://httpbin.org/ip").unwrap().text().unwrap();
+	println!("{:#?}", resp);
 }
