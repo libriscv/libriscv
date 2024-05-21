3067ba7b8b06284fde445413279604ff6fd48261
Alf-André Walla <fwsgonzo@hotmail.com>
2024-05-21 14:45:38 +0200

    Store and restore the compiler for gamedev example

--- examples/gamedev/build_and_run.sh
+++ examples/gamedev/build_and_run.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 set -e
 
+export ENGINE_CC="$CC"
+export ENGINE_CXX="$CXX"
+
+# The script program uses a different compiler
 pushd script_program
 source ./build.sh
 popd
+
+# Restore the original compiler
+CC=$ENGINE_CC
+CXX=$ENGINE_CXX
 
 mkdir -p .build
 pushd .build
