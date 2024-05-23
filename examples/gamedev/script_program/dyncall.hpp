ba1cd82cc65be0e785ef8e6c9a0ad9d192e4412d
Alf-André Walla <fwsgonzo@hotmail.com>
2024-05-23 12:10:13 +0200

    Simplify DEFINE_DYNCALL macro

--- examples/gamedev/script_program/dyncall.hpp
+++ examples/gamedev/script_program/dyncall.hpp
@@ -3,18 +3,14 @@
 
 #define DEFINE_DYNCALL(number, name, type) \
 	asm(".pushsection .text\n" \
-		".global sys_" #name "\n" \
+		".func sys_" #name "\n" \
 		"sys_" #name ":\n" \
 		"	li t0, " #number "\n" \
 		"   li a7, 510\n" \
 		"   ecall\n" \
 		"   ret\n"   \
-		".popsection .text\n" \
-		".pushsection .data\n" \
-		"sys_" #name "_end:\n"     \
-		".string \"" #name "\"\n" \
-		".byte 0\n"               \
-		".popsection .data\n"); \
+		".endfunc\n" \
+		".popsection .text\n"); \
 	using name##_t = type; \
 	extern "C" __attribute__((used, retain)) void sys_##name(); \
 	template <typename... Args> \
