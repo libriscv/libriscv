f8617d863bb703e5a092cc535836f2b018404fad
Alf-André Walla <fwsgonzo@hotmail.com>
2024-05-26 08:05:06 +0200

    Share API between Nelua and Nim examples, add Nelua debug example

--- examples/gamedev/nelua_program/build.sh
+++ examples/gamedev/nelua_program/build.sh
@@ -6,5 +6,12 @@ source="${1:-program}"
 cfile=.build/output.c
 binfile=.build/output.elf
 mkdir -p .build
+WRAP="-Wl,--wrap=malloc,--wrap=free,--wrap=calloc,--wrap=realloc,--wrap=memcpy,--wrap=memset,--wrap=memmove,--wrap=memcmp,--wrap=strlen,--wrap=strcmp,--wrap=strncmp"
 
-VERBOSE=1 nelua --ldflags="-static" --cflags="api.c -O2 -g3 -Wall -Wextra" -o $binfile $source.nelua
+if [ -z "$GDB" ]; then
+	echo "Compiling for Release"
+	nelua --ldflags="-static $WRAP" --cflags="api.c -O2 -g3 -Wall -Wextra" --release -o $binfile $source.nelua
+else
+	echo "Compiling for Debug with Remote GDB"
+	nelua --ldflags="-static" --cflags="api.c -O1 -g3 -Wall -Wextra" --debug -o $binfile $source.nelua
+fi
