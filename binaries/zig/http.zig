095c403879d7f7588db775f9329c0c78bd499864
Alf-André Walla <fwsgonzo@hotmail.com>
2024-06-07 22:08:55 +0200

    Update Zig HTTP example to Zig 0.13

--- binaries/zig/http.zig
+++ binaries/zig/http.zig
@@ -13,23 +13,20 @@ pub fn main() !void {
     // we can `catch unreachable` here because we can guarantee that this is a valid url.
     const uri = std.Uri.parse("https://example.com/") catch unreachable;
 
-    // these are the headers we'll be sending to the server
-    var headers = std.http.Headers{ .allocator = alloc };
-    defer headers.deinit();
-
-    try headers.append("accept", "*/*"); // tell the server we'll accept anything
+	var read_buffer: [8000]u8 = undefined;
 
     // make the connection and set up the request
-    var req = try client.open(.GET, uri, headers, .{});
+    var req = try client.open(.GET, uri, .{
+        .server_header_buffer = &read_buffer,
+        .extra_headers = &.{
+            .{ .name = "cookie", .value = "RISCV=awesome" },
+        },
+	});
     defer req.deinit();
 
     // send the request and headers to the server.
-    try req.send(.{});
+    try req.send();
     try req.wait();
-
-    // read the content-type header from the server, or default to text/plain
-    const content_type = req.response.headers.getFirstValue("content-type") orelse "text/plain";
-    _ = content_type;
 
     // read the entire response body
     const body = req.reader().readAllAlloc(alloc, 1024 * 1024) catch unreachable;
