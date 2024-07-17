b8f4d77469f3c24bb676c5e67b243cc28412de22
Alf-André Walla <fwsgonzo@hotmail.com>
2024-07-17 09:42:23 +0200

    Make D00M example easier to build

--- examples/doom/build.sh
+++ examples/doom/build.sh
@@ -4,11 +4,9 @@ set -e
 mkdir -p build
 pushd build
 if [ ! -f CMakeCache.txt ]; then
-	cmake .. -DCMAKE_BUILD_TYPE=MinSizeRel -DRISCV_BINARY_TRANSLATION=ON -DRISCV_LIBTCC=ON
+	cmake .. -DCMAKE_BUILD_TYPE=MinSizeRel -DRISCV_BINARY_TRANSLATION=ON
 fi
 make -j6
 popd
-ln -fs build/libtcc1.a .
 
-#NO_TRANSLATE=1 ./build/rvdoom
 VERBOSE=1 ./build/rvdoom
